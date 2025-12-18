7e01835dcf82960db6e31b08b8f0bfe27aa670c4
Alif <newst@pc.ecx.com>
2025-12-18 17:13:52 +0300

    fix(activity-comment): activity comment dynamic data

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,7 +1,7 @@
-//export const BASE_URL = "http://localhost:5000";
-export const BASE_URL = "http://196.189.247.228/api";
-export const SOCKET_URL = "http://196.189.247.228:5001";
-//export const SOCKET_URL = "http://localhost:5001";
+export const BASE_URL = "http://localhost:5000/api";
+// export const BASE_URL = "http://196.189.247.228/api";
+// export const SOCKET_URL = "http://196.189.247.228:5001";
+export const SOCKET_URL = "http://localhost:5001";
 
 export const PERMISSIONS = {
   // user permission
